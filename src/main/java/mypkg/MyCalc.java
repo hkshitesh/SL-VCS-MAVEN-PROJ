c70f44aaf75a6afd6ab032f827650d4fe187f533
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-06-21 10:40:04 +0530

    Prod Branch

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -17,6 +17,10 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public void show()
+	{
+		System.out.println("This is Prod Branch");
+	}
 
 	public static void main(String[] args) {
 
@@ -25,5 +29,6 @@ public class MyCalc {
 		System.out.println("Diff is "+calc.diff(20, 10));
 		System.out.println("Mul is "+calc.mul(20, 10));
 		System.out.println("Div is "+calc.div(20, 10));
+		calc.show();
 	}
 }
